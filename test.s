15efc2bf1130e6b9a1bcc1e84775fb4b88e75af2
BuildTools <unconfigured@null.spigotmc.org>
2023-09-09 22:00:03 +0100

    Updated test.pang

--- test.s
+++ test.s
@@ -4,96 +4,43 @@ default rel
 segment .text
     global main
 
-    extern ExitProcess, fwrite, fopen, fclose, strlen
+    extern ExitProcess, printf
 
 main:
+    ; INT - "lib\coreprt.pang" line 4
+    push    0
+
+    ; INT - "lib\coreprt.pang" line 4
+    push    1
+
+    ; INT - "lib\coreprt.pang" line 4
+    push    2
+
+    ; INT - "lib\coreprt.pang" line 4
+    push    3
+
+    ; INT - "test.pang" line 3
+    push    3
+
     ; STR - "test.pang" line 3
     lea     rax, [string_0]
     push    rax
 
-    ; STR - "test.pang" line 3
+    ; STR - "test.pang" line 5
     lea     rax, [string_1]
     push    rax
 
-    ; CALL - "test.pang" line 3
-    mov     rcx, [rsp]
-    mov     rdx, [rsp + 8]
-    mov     r8, [rsp + 16]
-    mov     r9, [rsp + 24]
-    call    fopen
-    push    rax
-
-    ; STR - "test.pang" line 5
-    lea     rax, [string_2]
-    push    rax
-
     ; CALL - "test.pang" line 5
     mov     rcx, [rsp]
     mov     rdx, [rsp + 8]
     mov     r8, [rsp + 16]
     mov     r9, [rsp + 24]
-    call    strlen
-    push    rax
-
-    ; SWAP - "test.pang" line 5
-    pop     rax
-    pop     rbx
-    push    rax
-    push    rbx
-
-    ; INT - "test.pang" line 5
-    push    1
-
-    ; SWAP - "test.pang" line 5
-    pop     rax
-    pop     rbx
-    push    rax
-    push    rbx
-
-    ; CALL - "test.pang" line 5
-    mov     rcx, [rsp]
-    mov     rdx, [rsp + 8]
-    mov     r8, [rsp + 16]
-    mov     r9, [rsp + 24]
-    call    fwrite
-    push    rax
-
-    ; DROP - "test.pang" line 5
-    pop     rax
-
-    ; SWAP - "test.pang" line 5
-    pop     rax
-    pop     rbx
-    push    rax
-    push    rbx
-
-    ; DROP - "test.pang" line 5
-    pop     rax
-
-    ; SWAP - "test.pang" line 5
-    pop     rax
-    pop     rbx
-    push    rax
-    push    rbx
-
-    ; DROP - "test.pang" line 5
-    pop     rax
-
-    ; DROP - "test.pang" line 6
-    pop     rax
-
-    ; CALL - "test.pang" line 8
-    mov     rcx, [rsp]
-    mov     rdx, [rsp + 8]
-    mov     r8, [rsp + 16]
-    mov     r9, [rsp + 24]
-    call    fclose
+    call    printf
     push    rax
 
     xor     rax, rax
     call    ExitProcess
 
 segment .data
-    string_0 db 119,0
-    string_1 db 116,101,115,116,46,116,120,116,0
-    string_2 db 72,101,108,108,111,44,32,119,111,114,108,100,33,10,0
+    string_0 db 116,101,115,116,46,112,97,110,103,0
+    string_1 db 37,115,32,37,100,32,37,100,32,37,100,32,37,100,32,37,100,10,0
